5242f79659e187568ab5890a341b893c3483fe52
Alex Blaylock <129992589+AlexBlaylock@users.noreply.github.com>
2023-07-04 15:36:31 -0500

    adding readme and finished javascript

--- Develop/script.js
+++ Develop/script.js
@@ -13,8 +13,9 @@ let pwLength = prompt("Enter password length. 8(min) to 128(max).")
 
 if (pwLength < 8 || pwLength > 128) {
 alert("Please enter a password length that is 8(min) to 128(max)!")
+return ""; 
 }
-
+// confirm prompts for what character sets to include.
 let lowercaseConfirm = confirm("Confirm to innclude lowercase letters?")
 let uppercaseConfirm = confirm("Confirm to include uppercase letters?")
 let numberConfirm = confirm("Confirm to include numbers?")
@@ -23,8 +24,9 @@ let specialConfirm = confirm("Confirm to include special characters.")
 // Alerts if you select none of the confirm options.
 if (!lowercaseConfirm && !uppercaseConfirm && !numberConfirm && !specialConfirm){
   alert("You need to select at least one character type!")
+  return "";
 }
-
+// These add the character sets to Pool for math function later.
 if (lowercaseConfirm) {
   emptyPool += lowercaseChars
 }
@@ -41,6 +43,15 @@ if (specialConfirm) {
   emptyPool += specialChars
 }
 
+let emptyPw = "" ;
+
+for (var i = 0; i < pwLength; i++) {
+  let randomIndex = Math.floor(Math.random() * emptyPool.length)
+  emptyPw += emptyPool.charAt(randomIndex);
+}
+
+return emptyPw; 
+// After trial and error I found that having a return at the end fixed my password coming as undefined.
 }
 
 // Write password to the #password input
